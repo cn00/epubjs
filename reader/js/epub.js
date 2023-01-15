11e0db5ada8c82b174923a407594c5f6d9578f1a
cn <cool_navy@qq.com>
2023-01-15 23:34:34 +0800

    add header Sec-Fetch-Mode/Site

--- reader/js/epub.js
+++ reader/js/epub.js
@@ -3449,6 +3449,8 @@ function request(url, type, withCredentials, headers) {
 	}
 	// 跨域支持
 	xhr.setRequestHeader("Accept", "*/*");
+	xhr.setRequestHeader("Sec-Fetch-Mode", "no-cors");
+	xhr.setRequestHeader("Sec-Fetch-Site", "cross-site");
 	xhr.setRequestHeader("Host", "snn-udi-s3.s3.cn-northwest-1.amazonaws.com.cn");
 	xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
 	xhr.setRequestHeader("Access-Control-Allow-Methods", "POST,GET,PUT,DELETE");
